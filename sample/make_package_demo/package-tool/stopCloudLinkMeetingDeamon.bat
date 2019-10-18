8057b419ee8d8dbfea8fd24fdd0629f791eca192
wudingyuan <wudingyuan@huawei.com>
2019-10-18 16:58:58 +0800

    Supporting the 19.1.12.1 version, demo refresh release.
    配套19.1.12.1版本，Demo刷新发布，主要更新包括：
    1. 解决部分问题
    
    Supporting the 19.1.12.1 version, demo refresh release. The main updates are as follows:
    1. Resolve some problems

--- sample/make_package_demo/package-tool/stopCloudLinkMeetingDeamon.bat
+++
@@ -1,6 +0,0 @@
-echo off
-rem sc delete tup_service_d
-::sc delete tup_service_d
-taskkill /f /t /im CloudLinkMeetingDeamon.exe
-taskkill /f /t /im CloudLinkMeetingService.exe
-taskkill /f /t /im tsdk_attach_agent.exe
